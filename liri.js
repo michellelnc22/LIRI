27c06387106e9f99c78878161dd9f7e80b83b77b
michellelnc22 <michellelnc22@gmail.com>
2019-07-31 16:26:58 -0400

    fixed some bugs

--- liri.js
+++ liri.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 
 var keys = require("./keys"); 
 
+var fs = require("fs"); 
+
+var axios = require("axios"); 
+
 var spotify = new spotify(keys.spotify); 
 
 var moment = require("moment"); 
 
-var pick = function(command) {
+var command = process.argv[2]; 
+
+var userSearch = process.argv.slice(3).join(" "); 
+
 
     switch (command) {
         case "concert-this": 
@@ -24,17 +31,16 @@ var pick = function(command) {
         default: 
         console.log("I don't recognize that command. Try again"); 
     }
-}
 
 var divider = "\n-----------------\n\n"; 
 
-var getMovie = function(movie) {
+var getMovie = function() {
 
-    if (movie === undefined) {
-        movie = "Mr. Nobody"; 
+    if (userSearch === undefined) {
+        userSearch = "Mr. Nobody"; 
     }
 
-    var URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=full&tomatoes=true&apikey=trilogy"; 
+    var URL = "https://www.omdbapi.com/?t=" + userSearch + "&y=&plot=full&tomatoes=true&apikey=trilogy"; 
     axios.get(URL).then(function(response) {
         var jsonData = response.data; 
 
@@ -56,8 +62,8 @@ var getMovie = function(movie) {
     });    
 }; 
 
-function getConcert(artist) {
-    var URL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+function getConcert() {
+    var URL = "https://rest.bandsintown.com/artists/" + userSearch + "?app_id=codingbootcamp";
     axios.get(URL).then(function(response) {
       
         for (i = 0; i < response.data.length; i++) {
@@ -79,23 +85,21 @@ function getConcert(artist) {
     })
 }
 
-function getSpotify(song) {
+function getSpotify() {
 
-  if (song === undefined) {
-     song = "The Sign"
+  if (userSearch === undefined) {
+     userSearch = "The Sign"
   }; 
   
   spotify.search({
       type: "track", 
-      query: song
+      query: userSearch
   }, function (error, data) {
    if (error) {
       return console.log ("Error!" + error); 
    }  
 
-   var spotifyArray = data.tracks.items; 
-
-   for (i = 0; i < spotifyArray.length; i++) {
+   for (i = 0; i < data.tracks.items.length; i++) {
 
     console.log(divider);
     console.log("Artist: " + data.tracks.items[i].artists[0].name); 
@@ -110,9 +114,9 @@ function getSpotify(song) {
 }
 
 function getRandom() {
-    fs.readFile("./random.txt", "utf8", (error, data) => {
+    fs.readFile("./random.txt", "utf8", (err, data) => {
         if (err) throw err; 
-        var file = data.split(","); 
+        var fileContent = data.split(","); 
         console.log(fileContent[0] + " " + fileContent[1]); 
         userSearch = fileContent[1]
         switch (fileContent[0]) {
